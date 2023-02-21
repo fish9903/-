9962e3d888d6245069f97c08835bda65f20256f9
KwangHo Cho <fishcopy6702@gmail.com>
2023-02-21 21:39:15 +0900

    update test

--- Example/test/Source.cpp
+++ Example/test/Source.cpp
@@ -1,41 +1,49 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <cmath>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+
+#define pii pair<int,int>
 
 using namespace std;
 
-vector<int> solution(vector<string> words, vector<string> queries) {
-    vector<int> answer;
-    map<string, int> m; // queries, cnt
-    for (string& s : queries)
-        m[s] = 0;
+int solution(int storey) {
+    int answer = 0;
+    string s = to_string(storey);
+    int digit = s.length(); // 자릿수
+    int div1 = pow(10, digit - 1);
+    int div2 = pow(10, digit);
+    vector<int> v; // cnt
 
-    for (string& s : words) {
-        string pre = s;
-        // prefix
-        for (int i = 0; i < pre.length(); ++i) {
-            pre[i] = '?';
-            if (m.find(pre) != m.end()) // find
-                m[pre]++;
+    queue<pii> q;
+    int cnt = 0;
+    q.push({ storey, cnt }); // num, cnt
+    while (!q.empty()) {
+        int num = q.front().first;
+        int cnt = q.front().second;
+        q.pop();
+
+        if (num < 10) {
+            v.push_back(cnt + num);
+            v.push_back(cnt + 10 - num + 1); // 반올림: + 1
         }
-        // postfix
-        pre = s;
-        for (int i = pre.length(); i > 0; --i) {
-            pre[i] = '?';
-            if (m.find(pre) != m.end()) // find
-                m[pre]++;
+        else {
+            int remain = num % 10;
+            num /= 10;
+            q.push({ num, cnt + remain });
+            q.push({ num + 1, cnt + 10 - remain });
         }
     }
+    sort(v.begin(), v.end());
 
-    for (string& s : queries)
-        answer.push_back(m[s]);
+    answer = v[0];
 
     return answer;
 }
-
 int main() {
-    vector<string> s = { "asdf" };
-    vector<string> ss = { "asd?" };
-    solution(s, ss);
+    cout << solution(555);
+
     return 0;
 }
