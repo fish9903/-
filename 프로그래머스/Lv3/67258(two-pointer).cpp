7b5e661aa8860e0e8ef004209f41ceb2a89134ff
KwangHo Cho <fishcopy6702@gmail.com>
2023-04-09 21:36:36 +0900

    add problem at Lv3

--- 프로그래머스/Lv3/67258(two-pointer).cpp
+++ 프로그래머스/Lv3/67258(two-pointer).cpp
@@ -11,49 +11,43 @@ map<string, int> m;
 
 vector<int> solution(vector<string> gems) {
     vector<int> answer;
-
+    
     int gem_s = gems.size();
-    if (gem_s == 1) return { 1, 1 };
-
+    if(gem_s == 1) return {1, 1};
+    
     set<string> gem_set(gems.begin(), gems.end());
     int set_s = gem_set.size();
-
+    
     int i, j;
-
+    
     int map_s;
     int start = 0, end = 0; // 2 pointer
     int dist = 1e9;
     int right = 1, left = 1;
-    while (end <= gem_s && start <= gem_s) {
+    while(1){
         map_s = m.size();
-
-        if (map_s == set_s) {
-            if (end - start < dist) {
+        
+        if(map_s == set_s){
+            if(end - start < dist){
                 right = end;
                 left = start;
                 dist = end - start;
             }
-
-            // start ����
+            
+            // start 증가
             m[gems[start]]--;
-            if (m[gems[start]] == 0) m.erase(gems[start]);
+            if(m[gems[start]] == 0) m.erase(gems[start]);
             start++;
         }
-        else {
+        else{
+            if(end == gem_s) break; // end에 접근하기 전에 검사 필요
             m[gems[end]]++;
             end++;
         }
     }
-
-    answer.push_back(left + 1);
+    
+    answer.push_back(left+1);
     answer.push_back(right);
-
+    
     return answer;
-}
-
-int main() {
-    solution({ "ZZZ", "YYY", "NNNN", "YYY", "BBB" });
-
-
-    return 0;
 }
